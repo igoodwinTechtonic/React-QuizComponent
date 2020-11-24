1ed91ca8b5aa9036358c0c1efa29c506ba4a276b
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-11-24 17:47:59 -0600

    working

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -10,9 +11,17 @@ class Quiz extends Component {
     }
   }
   render() {
+    const isQuizEnd = () => {
+      return ((this.state.quiz_position - 1 === quizData.quiz_questions.length)
+      ? 'true'
+      : 'false');
+    }
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+        {isQuizEnd()
+          ? <QuizEnd />
+          : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+        }
       </div>
     )
   }

09fe0eb45f0b837b12a549fa49ef352b3fec3439
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-11-24 16:58:18 -0600

    Initial commit

---
+++ src/Quiz.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quiz_position: 1,
+    }
+  }
+  render() {
+    console.log(quizData.quiz_questions[this.state.quiz_position])
+    return (
+      <div>
+        <div className="QuizQuestion">
+          <p>{quizData.quiz_questions[this.state.quiz_position]}</p>
+          <p>{quizData.quiz_questions[this.state.quiz_position].instruction_text}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Quiz;
